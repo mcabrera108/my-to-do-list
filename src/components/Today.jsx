4a83611465eb5fb23d2c777bf0d0c0386609a34c
mcabrera108 <martin.cabrera103@gmail.com>
2024-10-29 10:11:13 -0500

    Added Timezone functionality for Today URL

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -2,14 +2,19 @@ import Task from "./Task";
 import { useContext } from "react";
 import projectContext from "../context/projectContext";
 import style from "../style_modules/main.module.css";
+import { formatToTimeZone } from "date-fns-timezone";
 
 function Today() {
   const todayDate = new Date();
+  const format = "YYYY-MM-DD";
+  const output = formatToTimeZone(todayDate, format, {
+    timeZone: "America/Chicago",
+  });
   const { tasks } = useContext(projectContext);
   return (
     <div className={style.taskWrapper}>
       {tasks?.map((task) => {
-        if (todayDate.toISOString().substring(0, 10) === task.taskDate) {
+        if (output === task.taskDate) {
           return <Task indTask={task} key={task.id} />;
         }
       })}
